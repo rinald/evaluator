34720a0173cf54f46ceffc630f55b585739d617f
rinald <rinaldshabani@gmail.com>
2017-12-27 10:37:31 +0100

    Initial changes

--- tests.py
+++
@@ -1,9 +0,0 @@
-"""Tests of helper functions and structures."""
-
-import helpers
-import parenthesise
-
-def parenthesise_test():
-    filename = 'parenthesise-tests.txt'
-    function = parenthesise.parenthesise
-    helpers.test(filename, function)
