dcc23720968b45d82bd8972b5a1dcbf202d61302
rinald <rinaldshabani@gmail.com>
2017-12-30 14:31:53 +0100

    Rename elementart_expression to simple_expression and make small changes

---
+++ evaluator/simple_expression.py
@@ -0,0 +1,22 @@
+"""Defines a parser for simple expressions.
+
+A simple expression has no parentheses.
+"""
+
+class SimpleExpression:
+    """Defines a simple expression."""
+
+    def __init__(self, simple_expression):
+        pass
+    def __str__(self):
+        pass
+    def __repr__(self):
+        pass
+    def evaluate(self):
+        """Evaluate expression."""
+
+        pass
+    def reset(self, simple_expression):
+        """Reset expression."""
+
+        pass
