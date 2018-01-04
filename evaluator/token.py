9b510eaa93e5939a9d813671bf66fe00871eae77
rinald <rinaldshabani@gmail.com>
2018-01-04 10:43:33 +0100

    Added missing __repr__

--- evaluator/token.py
+++ evaluator/token.py
@@ -9,6 +9,7 @@ class Token:
     def __init__(self, type_, value):
         self.type = type_
         self.value = value
-
     def __str__(self):
         return "<token <{}> : '{}'>".format(self.type, self.value)
+    def __repr__(self):
+        return self.__str__()
